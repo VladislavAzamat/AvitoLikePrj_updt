c119b9c01c45b1ae003db4329fd5bb7316083ecd
csa21472001 <21461901as@gmail.com>
2024-03-18 18:41:03 +0300

    Week_6. Comments&ReadMe

--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
@@ -1,18 +1,15 @@
 package ru.skypro.homework.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
 
 @Data
 @Builder
 @AllArgsConstructor
-//@NoArgsConstructor
 public class CreateOrUpdateAd {
-    @NotBlank (message = "Title must not be blank")
+    @NotBlank(message = "Title must not be blank")
     private String title;
     @Min(0)
     private Integer price;
